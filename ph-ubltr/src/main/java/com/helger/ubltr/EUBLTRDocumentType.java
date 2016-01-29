fb32050e834497bdcebda2a570271c24986904df
Philip Helger <philip@helger.com>
2016-01-29 12:54:03 +0100

    Started document type support

---
+++ ph-ubltr/src/main/java/com/helger/ubltr/EUBLTRDocumentType.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.ubltr;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.xml.namespace.QName;
+import javax.xml.validation.Schema;
+
+import com.helger.ubl.api.IUBLDocumentType;
+import com.helger.ubl.api.UBLDocumentType;
+
+import tr.gov.efatura.useraccount.CancelUserAccountType;
+import tr.gov.efatura.useraccount.ProcessUserAccountType;
+
+/**
+ * Enumeration with all available UBLTR document types.
+ *
+ * @author Philip Helger
+ */
+public enum EUBLTRDocumentType implements IUBLDocumentType
+{
+  CANCEL_USER_ACCOUNT (CancelUserAccountType.class, "HRXML/UserAccount.xsd"),
+  PROCESS_USER_ACCOUNT (ProcessUserAccountType.class, "HRXML/UserAccount.xsd");
+
+  private final UBLDocumentType m_aDocType;
+
+  private EUBLTRDocumentType (@Nonnull final Class <?> aClass, @Nonnull final String sXSDPath)
+  {
+    m_aDocType = new UBLDocumentType (aClass, CUBLTR.SCHEMA_DIRECTORY + sXSDPath);
+  }
+
+  @Nonnull
+  public Class <?> getImplementationClass ()
+  {
+    return m_aDocType.getImplementationClass ();
+  }
+
+  @Nonnull
+  public Package getPackage ()
+  {
+    return m_aDocType.getPackage ();
+  }
+
+  @Nonnull
+  public String getLocalName ()
+  {
+    return m_aDocType.getLocalName ();
+  }
+
+  @Nonnull
+  public String getNamespaceURI ()
+  {
+    return m_aDocType.getNamespaceURI ();
+  }
+
+  @Nonnull
+  public QName getQName ()
+  {
+    return m_aDocType.getQName ();
+  }
+
+  @Nonnull
+  public String getXSDPath ()
+  {
+    return m_aDocType.getXSDPath ();
+  }
+
+  @Nonnull
+  public Schema getSchema (@Nullable final ClassLoader aClassLoader)
+  {
+    return m_aDocType.getSchema (aClassLoader);
+  }
+}
---
+++ ph-ubltr/src/main/java/com/helger/ubltr/UBLTRNamespaceContext.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.ubltr;
+
+import com.helger.sbdh.CSBDH;
+import com.helger.ubl21.UBL21NamespaceContext;
+
+/**
+ * The namespace context to be used as the namespace prefix mapper.
+ *
+ * @author Philip Helger
+ */
+public class UBLTRNamespaceContext extends UBL21NamespaceContext
+{
+  public UBLTRNamespaceContext ()
+  {
+    addMapping ("sh", CSBDH.SBDH_NS);
+    addMapping ("ef", "http://www.efatura.gov.tr/package-namespace");
+    addMapping ("oa", "http://www.openapplications.org/oagis/9");
+  }
+}
