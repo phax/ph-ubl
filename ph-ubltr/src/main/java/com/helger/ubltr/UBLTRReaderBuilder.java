23b6ad6d5ab97c4dd9fdce24b153024ef2e8f9c0
phax <philip@helger.com>
2016-04-22 08:55:03 +0200

    Updated license headers

--- ph-ubltr/src/main/java/com/helger/ubltr/UBLTRReaderBuilder.java
+++ ph-ubltr/src/main/java/com/helger/ubltr/UBLTRReaderBuilder.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
  * philip[at]helger[dot]com
  *
- * Licensed under the Apache License, VersionTR (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
