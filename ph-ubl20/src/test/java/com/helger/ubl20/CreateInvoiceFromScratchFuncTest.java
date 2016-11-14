41de9bcdeded284988da539f3219a532f9bf8271
Philip Helger <philip@helger.com>
2016-11-14 13:39:52 +0100

    Added example with extension

--- ph-ubl20/src/test/java/com/helger/ubl20/CreateInvoiceFromScratchFuncTest.java
+++ ph-ubl20/src/test/java/com/helger/ubl20/CreateInvoiceFromScratchFuncTest.java
@@ -6,15 +6,20 @@ import java.io.File;
 import java.math.BigDecimal;
 
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
 import com.helger.commons.state.ESuccess;
 import com.helger.datetime.util.PDTXMLConverter;
+import com.helger.xml.serialize.read.DOMReader;
 
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.CustomerPartyType;
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.InvoiceLineType;
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.ItemType;
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.MonetaryTotalType;
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.SupplierPartyType;
+import oasis.names.specification.ubl.schema.xsd.commonextensioncomponents_2.ExtensionContentType;
+import oasis.names.specification.ubl.schema.xsd.commonextensioncomponents_2.UBLExtensionType;
+import oasis.names.specification.ubl.schema.xsd.commonextensioncomponents_2.UBLExtensionsType;
 import oasis.names.specification.ubl.schema.xsd.invoice_2.InvoiceType;
 import un.unece.uncefact.codelist.specification._54217._2001.CurrencyCodeContentType;
 
@@ -55,6 +60,49 @@ public final class CreateInvoiceFromScratchFuncTest
     aInvoice.addInvoiceLine (aLine);
 
     final ESuccess eSuccess = UBL20Writer.invoice ().write (aInvoice, new File ("target/dummy-invoice.xml"));
+    assertTrue (eSuccess.isSuccess ());
+  }
+
+  @Test
+  public void testCreateInvoiceFromScratchWithCustomNamespace () throws SAXException
+  {
+    final CurrencyCodeContentType eCurrency = CurrencyCodeContentType.EUR;
+
+    final InvoiceType aInvoice = new InvoiceType ();
+    aInvoice.setID ("Dummy Invoice number");
+    aInvoice.setIssueDate (PDTXMLConverter.getXMLCalendarDateNow ());
+
+    final SupplierPartyType aSupplier = new SupplierPartyType ();
+    aInvoice.setAccountingSupplierParty (aSupplier);
+
+    final CustomerPartyType aCustomer = new CustomerPartyType ();
+    aInvoice.setAccountingCustomerParty (aCustomer);
+
+    final MonetaryTotalType aMT = new MonetaryTotalType ();
+    aMT.setPayableAmount (BigDecimal.TEN).setCurrencyID (eCurrency);
+    aInvoice.setLegalMonetaryTotal (aMT);
+
+    final InvoiceLineType aLine = new InvoiceLineType ();
+    aLine.setID ("1");
+
+    final ItemType aItem = new ItemType ();
+    aLine.setItem (aItem);
+
+    aLine.setLineExtensionAmount (BigDecimal.TEN).setCurrencyID (eCurrency);
+
+    aInvoice.addInvoiceLine (aLine);
+
+    // Add extension
+    final UBLExtensionsType aExtensions = new UBLExtensionsType ();
+    final UBLExtensionType aExtension = new UBLExtensionType ();
+    final ExtensionContentType aExtensionContent = new ExtensionContentType ();
+    aExtensionContent.setAny (DOMReader.readXMLDOM ("<root xmlns='urn:sunat:names:specification:ubl:peru:schema:xsd:SunatAggregateComponents-1'>test</root>")
+                                       .getDocumentElement ());
+    aExtension.setExtensionContent (aExtensionContent);
+    aExtensions.addUBLExtension (aExtension);
+    aInvoice.setUBLExtensions (aExtensions);
+
+    final ESuccess eSuccess = UBL20Writer.invoice ().write (aInvoice, new File ("target/dummy-invoice-with-ext.xml"));
     assertTrue (eSuccess.isSuccess ());
   }
 }
