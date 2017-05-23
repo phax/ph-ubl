2020501f32d45c5c7deac3ae4ecb0a186c8ef864
Philip Helger <philip@helger.com>
2017-05-23 15:37:36 +0200

    Added example for #10

--- ph-ubl21/src/test/java/com/helger/ubl21/CreateInvoiceFromScratchFuncTest.java
+++ ph-ubl21/src/test/java/com/helger/ubl21/CreateInvoiceFromScratchFuncTest.java
@@ -22,15 +22,22 @@ import java.io.File;
 import java.math.BigDecimal;
 
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 import com.helger.commons.state.ESuccess;
 import com.helger.datetime.util.PDTXMLConverter;
+import com.helger.xml.XMLFactory;
+import com.helger.xml.namespace.MapBasedNamespaceContext;
 
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_21.CustomerPartyType;
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_21.InvoiceLineType;
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_21.ItemType;
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_21.MonetaryTotalType;
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_21.SupplierPartyType;
+import oasis.names.specification.ubl.schema.xsd.commonextensioncomponents_21.ExtensionContentType;
+import oasis.names.specification.ubl.schema.xsd.commonextensioncomponents_21.UBLExtensionType;
+import oasis.names.specification.ubl.schema.xsd.commonextensioncomponents_21.UBLExtensionsType;
 import oasis.names.specification.ubl.schema.xsd.invoice_21.InvoiceType;
 
 /**
@@ -45,7 +52,10 @@ public final class CreateInvoiceFromScratchFuncTest
   {
     final String sCurrency = "EUR";
 
+    // Create domain object
     final InvoiceType aInvoice = new InvoiceType ();
+
+    // Fill it
     aInvoice.setID ("Dummy Invoice number");
     aInvoice.setIssueDate (PDTXMLConverter.getXMLCalendarDateNow ());
 
@@ -69,7 +79,84 @@ public final class CreateInvoiceFromScratchFuncTest
 
     aInvoice.addInvoiceLine (aLine);
 
+    // Write to disk
     final ESuccess eSuccess = UBL21Writer.invoice ().write (aInvoice, new File ("target/dummy-invoice.xml"));
     assertTrue (eSuccess.isSuccess ());
+  }
+
+  /**
+   * This is an example that creates an XML schema compliant invoice <b>AND</b>
+   * adds an extension as requested in issue #10
+   */
+  @Test
+  public void testCreateInvoiceFromScratchWithExtension ()
+  {
+    final String sCurrency = "EUR";
+
+    // Create domain object
+    final InvoiceType aInvoice = new InvoiceType ();
+
+    // Fill it
+    aInvoice.setID ("Dummy Invoice number");
+    aInvoice.setIssueDate (PDTXMLConverter.getXMLCalendarDateNow ());
+
+    final SupplierPartyType aSupplier = new SupplierPartyType ();
+    aInvoice.setAccountingSupplierParty (aSupplier);
+
+    final CustomerPartyType aCustomer = new CustomerPartyType ();
+    aInvoice.setAccountingCustomerParty (aCustomer);
+
+    final MonetaryTotalType aMT = new MonetaryTotalType ();
+    aMT.setPayableAmount (BigDecimal.TEN).setCurrencyID (sCurrency);
+    aInvoice.setLegalMonetaryTotal (aMT);
+
+    final InvoiceLineType aLine = new InvoiceLineType ();
+    aLine.setID ("1");
+
+    final ItemType aItem = new ItemType ();
+    aLine.setItem (aItem);
+
+    aLine.setLineExtensionAmount (BigDecimal.TEN).setCurrencyID (sCurrency);
+
+    aInvoice.addInvoiceLine (aLine);
+
+    // Example extension content from issue #10:
+    /**
+     * <pre>
+       <aife:FactureExtension xmlns:aife="urn:AIFE:Facture:Extension">
+         <aife:CategoryCode>XX</aife:CategoryCode>
+       </aife:FactureExtension>
+     * </pre>
+     */
+    final Document aDoc = XMLFactory.newDocument ();
+    final String sNamespaceURI = "urn:AIFE:Facture:Extension";
+    final Node eRoot = aDoc.appendChild (aDoc.createElementNS (sNamespaceURI, "FactureExtension"));
+    final Node eCategoryCode = eRoot.appendChild (aDoc.createElementNS (sNamespaceURI, "CategoryCode"));
+    eCategoryCode.appendChild (aDoc.createTextNode ("XX"));
+
+    // Now add the extension
+    final UBLExtensionsType aExtensions = new UBLExtensionsType ();
+    final UBLExtensionType aExtension = new UBLExtensionType ();
+    final ExtensionContentType aExtensionContent = new ExtensionContentType ();
+    // Add the root element - NEVER use the whole document!
+    aExtensionContent.setAny (aDoc.getDocumentElement ());
+    aExtension.setExtensionContent (aExtensionContent);
+    aExtensions.addUBLExtension (aExtension);
+    aInvoice.setUBLExtensions (aExtensions);
+
+    // Write to disk ("as is")
+    ESuccess eSuccess = UBL21Writer.invoice ().write (aInvoice, new File ("target/dummy-invoice-with-extension.xml"));
+    assertTrue (eSuccess.isSuccess ());
+
+    // doesn't work as expected yet
+    if (false)
+    {
+      // Write to disk with a custom namespace prefix mapping
+      final UBL21WriterBuilder <InvoiceType> aBuilder = UBL21Writer.invoice ();
+      final MapBasedNamespaceContext aCtx = (MapBasedNamespaceContext) aBuilder.getNamespaceContext ();
+      aCtx.addMapping ("aife", sNamespaceURI);
+      eSuccess = aBuilder.write (aInvoice, new File ("target/dummy-invoice-with-extension-and-ns.xml"));
+      assertTrue (eSuccess.isSuccess ());
+    }
   }
 }
