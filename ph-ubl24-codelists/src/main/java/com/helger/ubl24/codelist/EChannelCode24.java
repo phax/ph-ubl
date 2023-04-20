7d4e0d028dbdd26b5eca77d63750f5bf95c8ae79
Philip Helger <philip@helger.com>
2023-04-20 23:41:20 +0200

    Initial version UBL 2.4 codelists

---
+++ ph-ubl24-codelists/src/main/java/com/helger/ubl24/codelist/EChannelCode24.java
@@ -0,0 +1,108 @@
+package com.helger.ubl24.codelist;
+
+import com.helger.commons.annotation.CodingStyleguideUnaware;
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.id.IHasID;
+import com.helger.commons.lang.EnumHelper;
+import com.helger.commons.name.IHasDisplayName;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+
+/**
+ * This file was automatically generated from Genericode file ChannelCode-2.4.gc. Do NOT edit!
+ * It contains a total of 47 entries!
+ * @author com.helger.ubl24.supplementary.tools.MainCreateEnumsGenericode24
+ */
+@CodingStyleguideUnaware
+public enum EChannelCode24
+    implements IHasID<String> , IHasDisplayName
+{
+    AA("AA", "Circuit switching", "A process that, on demand, connects two or more data terminal equipments and permits the exclusive use of a data circuit between them until the connection is released (ISO)."),
+    AB("AB", "SITA", "Communications number assigned by Societe Internationale de Telecommunications Aeronautiques (SITA)."),
+    AC("AC", "ARINC", "Communications number assigned by Aeronautical Radio Inc."),
+    AD("AD", "AT&T mailbox", "AT&T mailbox identifier."),
+    AE("AE", "Peripheral device", "Peripheral device identification."),
+    AF("AF", "U.S. Defense Switched Network", "The switched telecommunications network of the United States Department of Defense."),
+    AG("AG", "U.S. federal telecommunications system", "The switched telecommunications network of the United States government."),
+    AH("AH", "World Wide Web", "Data exchange via the World Wide Web."),
+    AI("AI", "International calling country code", "Identifies that portion of an international telephone number representing the country code to be used when calling internationally."),
+    AJ("AJ", "Alternate telephone", "Identifies the alternate telephone number."),
+    AK("AK", "Videotex number", "Code that identifies the communications number for the online videotex service."),
+    AL("AL", "Cellular phone", "Identifies the cellular phone number."),
+    AM("AM", "International telephone direct line", "The international telephone direct line number."),
+    AN("AN", "O.F.T.P. (ODETTE File Transfer Protocol)", "ODETTE File Transfer Protocol."),
+    AO("AO", "Uniform Resource Location (URL)", "Identification of the Uniform Resource Location (URL) Synonym: World wide web address."),
+    AP("AP", "Very High Frequency (VHF) radio telephone", "VHF radio telephone."),
+    AQ("AQ", "X.400 address for mail text", "The X.400 address accepting information in the body text of a message."),
+    AR("AR", "AS1 address", "Address capable of receiving messages in accordance with the EDIINT/AS1 protocol for MIME based EDI ."),
+    AS("AS", "AS2 address", "Address capable of receiving messages in accordance with the EDIINT/AS2 protocol."),
+    AT("AT", "AS3 address", "Address capable of receiving messages in accordance with the EDIINT/AS3 protocol."),
+    AU("AU", "File Transfer Protocol", "Address capable for receiving message in accordance with the File Transfer Protocol (IETF RFC 959 et. al.)."),
+    AV("AV", "Inmarsat call number", "Contact number based on Inmarsat."),
+    AW("AW", "Radio communication call sign", "Contact number for radio communication based on call sign."),
+    CA("CA", "Cable address", "The communication number identifies a cable address."),
+    EI("EI", "EDI transmission", "Number identifying the service and service user."),
+    EM("EM", "Electronic mail", "Exchange of mail by electronic means."),
+    EX("EX", "Extension", "Telephone extension."),
+    FT("FT", "File transfer access method", "According to ISO."),
+    FX("FX", "Telefax", "Device used for transmitting and reproducing fixed graphic material (as printing) by means of signals over telephone lines or other electronic transmission media."),
+    GM("GM", "GEIS (General Electric Information Service) mailbox", "The communication number identifies a GEIS mailbox."),
+    IE("IE", "IBM information exchange", "The communication number identifies an IBM IE mailbox."),
+    IM("IM", "Internal mail", "Internal mail address/number."),
+    MA("MA", "Mail", "Postal service document delivery."),
+    PB("PB", "Postbox number", "The communication number identifies a postbox."),
+    PS("PS", "Packet switching", "The process of routing and transferring data by means of addressed packets so that a channel is occupied only during the transmission; upon completion of the transmission the channel is made available for the transfer of other packets (ISO)."),
+    SW("SW", "S.W.I.F.T.", "Communications address assigned by Society for Worldwide Interbank Financial Telecommunications s.c."),
+    TE("TE", "Telephone", "Voice/data transmission by telephone."),
+    TG("TG", "Telegraph", "Text transmission via telegraph."),
+    TL("TL", "Telex", "Transmission of text/data via telex."),
+    TM("TM", "Telemail", "Transmission of text/data via telemail."),
+    TT("TT", "Teletext", "Transmission of text/data via teletext."),
+    TX("TX", "TWX", "Communication service involving Teletypewriter machines connected by wire or electronic transmission media. Teletypewriter machines are the devices used to send and receive signals and produce hardcopy from them."),
+    XF("XF", "X.400 address", "The X.400 address."),
+    XG("XG", "Pager", "Identifies that the communication number is for a pager."),
+    XH("XH", "International telephone switchboard", "The international telephone switchboard number."),
+    XI("XI", "National telephone direct line", "The national telephone direct line number."),
+    XJ("XJ", "National telephone switchboard", "The national telephone switchboard number.");
+    public static final String AGENCY_ID = "6";
+    public static final String AGENCY_LONG_NAME = "United Nations Economic Commission for Europe";
+    public static final String LIST_ID = "UN/EDIFACT EDED 3155";
+    public static final String LIST_VERSION = "18B";
+    private final String m_sID;
+    private final String m_sDisplayName;
+    private final String m_sDescription;
+
+    EChannelCode24(@Nonnull @Nonempty final String sID, @Nonnull final String sDisplayName, @Nullable final String sDescription) {
+        m_sID = sID;
+        m_sDisplayName = sDisplayName;
+        m_sDescription = sDescription;
+    }
+
+    @Nonnull
+    @Nonempty
+    public String getID() {
+        return m_sID;
+    }
+
+    @Nonnull
+    public String getDisplayName() {
+        return m_sDisplayName;
+    }
+
+    @Nullable
+    public String getDescription() {
+        return m_sDescription;
+    }
+
+    @Nullable
+    public static EChannelCode24 getFromIDOrNull(@Nullable final String sID) {
+        return EnumHelper.getFromIDOrNull(EChannelCode24 .class, sID);
+    }
+
+    @Nullable
+    public static String getDisplayNameFromIDOrNull(@Nullable final String sID) {
+        final EChannelCode24 eValue = EChannelCode24 .getFromIDOrNull(sID);
+        return ((eValue == null)?null:eValue.getDisplayName());
+    }
+}
---
+++ ph-ubl24-codelists/src/main/java/com/helger/ubl24/codelist/ETransportModeCode24.java
@@ -0,0 +1,71 @@
+package com.helger.ubl24.codelist;
+
+import com.helger.commons.annotation.CodingStyleguideUnaware;
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.id.IHasID;
+import com.helger.commons.lang.EnumHelper;
+import com.helger.commons.name.IHasDisplayName;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+
+/**
+ * This file was automatically generated from Genericode file TransportModeCode-2.4.gc. Do NOT edit!
+ * It contains a total of 10 entries!
+ * @author com.helger.ubl24.supplementary.tools.MainCreateEnumsGenericode24
+ */
+@CodingStyleguideUnaware
+public enum ETransportModeCode24
+    implements IHasID<String> , IHasDisplayName
+{
+    _0("0", "Transport mode not specified", "Transport mode has not been specified. Notes: 1) This code can be used when the mode is not known or when information on is not available at the time of issuing the document concerned."),
+    _1("1", "Maritime transport", "Transport of goods and/or persons is by sea."),
+    _2("2", "Rail transport", "Transport of goods and/or persons is by rail."),
+    _3("3", "Road transport", "Transport of goods and/or persons is by road."),
+    _4("4", "Air transport", "Transport of goods and/or persons is by air."),
+    _5("5", "Mail", "Method to convey goods is by mail. Notes: 1) This code is provided for practical reasons, despite the fact that mail is not a genuine mode of transport. In many countries, the value of merchandise exported by mail is considerable, but the exporter or importer concerned would be unable to state by which mode postal items had been conveyed."),
+    _6("6", "Multimodal transport", "Method to convey goods and/or persons is by multimodal transport. Notes: 1) This code is provided for practical reasons, despite the fact that multimodal transport is not a genuine mode of transport. It can be used when goods are carried by at least two different modes from a place at which the goods are taken in charge by a transport operator to a place designated for delivery, on the basis of one transport contract. (Operations of pick-up and delivery of goods carried out in the performance of a single mode of transport, as defined in such a contract, shall not be considered as multimodal transport)."),
+    _7("7", "Fixed transport installations", "Transport of item is via a fixed transport installation. Notes: 1) This code applies to installations for continuous transport such as pipelines, ropeways and electric power lines."),
+    _8("8", "Inland water transport", "Transport of goods and/or persons is by inland water transport."),
+    _9("9", "Transport mode not applicable", "The mode of transport is not applicable.");
+    public static final String AGENCY_ID = "6";
+    public static final String AGENCY_LONG_NAME = "United Nations Economic Commission for Europe";
+    public static final String LIST_ID = "UNECE Recommendation 19 - Codes for Modes of Transport";
+    public static final String LIST_VERSION = "18B";
+    private final String m_sID;
+    private final String m_sDisplayName;
+    private final String m_sDescription;
+
+    ETransportModeCode24(@Nonnull @Nonempty final String sID, @Nonnull final String sDisplayName, @Nullable final String sDescription) {
+        m_sID = sID;
+        m_sDisplayName = sDisplayName;
+        m_sDescription = sDescription;
+    }
+
+    @Nonnull
+    @Nonempty
+    public String getID() {
+        return m_sID;
+    }
+
+    @Nonnull
+    public String getDisplayName() {
+        return m_sDisplayName;
+    }
+
+    @Nullable
+    public String getDescription() {
+        return m_sDescription;
+    }
+
+    @Nullable
+    public static ETransportModeCode24 getFromIDOrNull(@Nullable final String sID) {
+        return EnumHelper.getFromIDOrNull(ETransportModeCode24 .class, sID);
+    }
+
+    @Nullable
+    public static String getDisplayNameFromIDOrNull(@Nullable final String sID) {
+        final ETransportModeCode24 eValue = ETransportModeCode24 .getFromIDOrNull(sID);
+        return ((eValue == null)?null:eValue.getDisplayName());
+    }
+}
